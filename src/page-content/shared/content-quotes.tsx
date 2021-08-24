770e9cd14b326a89480a1637503b0a4e659c081d
vinckr <vincent@ory.sh>
2021-08-24 15:01:34 +0200

    feat: more quotes

--- src/page-content/shared/content-quotes.tsx
+++ src/page-content/shared/content-quotes.tsx
@@ -38,3 +38,125 @@ export const sharedQuotes: QuoteProps = {
     }
   ]
 }
+
+export const otherQuotes: QuoteProps = {
+  id: 'other.quotes',
+  quotes: [
+    {
+      big: true,
+      description: (
+        <>
+          We chose the Ory Stack to develop an internal IAM. Ory is ideal because we
+          could customize it exactly how we needed to and could build a flexible system for
+          authorizing user actions.
+        </>
+      ),
+      person: `Dimitriy Gaevskiy`,
+      jobTitle: `Software Architect, Tinkoff Group`
+    },
+    {
+      description: (
+        <>
+          We enjoy using Ory because of the simplicity to integrate thanks to its
+          modularity, its active and vocal developer base, and the extent and ease by
+          which Ory tools can be configured.
+        </>
+      ),
+      person: `Matt Brown`,
+      jobTitle: `Bioinformatician, Dante Labs`
+    },
+    {
+      description: (
+        <>
+          We rely on Ory to be our Oauth provider and it fits perfectly into our
+          infrastructure, is hardened, and aligns with our principles. Ory is a great fit
+          for us.
+        </>
+      ),
+      person: `Benjamin Kristensen`,
+      jobTitle: `Senior Software Engineer, Enode`
+    },
+  ]
+}
+
+export const moreQuotes: QuoteProps = {
+  id: 'more.quotes',
+  quotes: [
+    {
+      big: true,
+      description: (
+        <>
+          At Discogs we use Ory to handle authentication and authorization for
+          over 8 million music enthusiasts across our applications. It allows us to focus on building the largest and most 
+          comprehensive music database and marketplace, rather than reinvent the wheel.
+        </>
+      ),
+      person: `Corey Burmeister`,
+      jobTitle: `Author, Discogs`
+    },
+    {
+      description: (
+        <>
+          We enjoy using Ory because of the simplicity to integrate thanks to its
+          modularity, its active and vocal developer base, and the extent and ease by
+          which Ory tools can be configured.
+        </>
+      ),
+      person: `Matt Brown`,
+      jobTitle: `Bioinformatician, Dante Labs`
+    },
+    {
+      description: (
+        <>
+          At Factly, we were looking for a lightweight alternative to Keycloak that
+          would meet all our IAM needs across multiple applications. Ory, written in
+          Golang was the best option out there which was simple and performant.
+        </>
+      ),
+      person: `Shashi Deshetti`,
+      jobTitle: `CoFounder, factly  `
+    },
+  ]
+}
+
+
+export const additionalQuotes: QuoteProps = {
+  id: 'additional.quotes',
+  quotes: [
+    {
+      big: true,
+      description: (
+        <>
+          At Discogs we use Ory to handle authentication and authorization for
+          over 8 million music enthusiasts across our applications. 
+          It allows us to focus on building the largest and most 
+          comprehensive music database and marketplace, rather than reinvent the wheel.
+        </>
+      ),
+      person: `Corey Burmeister`,
+      jobTitle: `Author, Discogs`
+    },    
+    {
+      description: (
+        <>
+          At Factly, we were looking for a lightweight alternative to Keycloak that
+          would meet all our IAM needs across multiple applications. Ory, written in
+          Golang was the best option out there that was simple and performant.
+        </>
+      ),
+      person: `Shashi Deshetti`,
+      jobTitle: `CoFounder, factly  `
+    },
+    {
+      description: (
+        <>
+          We choose Ory because it is open-source, does not force an UI on us, and is
+          micro services oriented. Ory matches how our organization works and being able
+          to build our own system is a big advantage compared to most other things.
+        </>
+      ),
+      person: `Jakob Sinclair`,
+      jobTitle: `Engineering, Pirat Partiet Sweden`
+    }
+  ]
+}
